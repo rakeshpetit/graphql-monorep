c4995ec4b831547170f5a3d408a2e217f1516daa
Rakesh Arunachalam <rakesh@thoughtbot.com>
2020-01-10 15:23:59 +0000

    Fix screen flicker and add progress indicator

---
+++ frontend/pages/_document.js
@@ -0,0 +1,25 @@
+import Document, { Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const page = ctx.renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps, ...page, styleTags };
+  }
+  render() {
+    return (
+      <html>
+        <Head>{this.props.styleTags}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
