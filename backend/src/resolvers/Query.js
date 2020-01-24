77bb76ab32c5c5e020ee9dc2016fe09f0e2c5cda
Rakesh Arunachalam <rakesh@thoughtbot.com>
2020-01-24 10:33:06 +0000

    Use forwardTo instead of querying database

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,12 +1,14 @@
+const { forwardTo } = require('prisma-binding')
 const Query = {
   // dogs(parent, args, ctx, info) {
   //     global.dogs = global.dogs || [];
   //     return global.dogs
   // }
-  async items(parent, args, ctx, info) {
-    const items = await ctx.db.query.items();
-    return items;
-  }
+  items: forwardTo('db')
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // }
 };
 
 module.exports = Query;
